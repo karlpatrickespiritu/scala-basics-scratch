2dd6b77f4ac5484910c2190294142eb9a4134c79
Developer <developer@VDX-MBA6.local>
2016-06-07 11:16:23 +0800

    tests

--- test1/src/main/scala/com/example/Hello.scala
+++ test1/src/main/scala/com/example/Hello.scala
@@ -1,7 +1,15 @@
+// https://www.youtube.com/watch?v=DzFt0YkZo8M
+
+import scala.io.StdIn.{ readLine, readInt }
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
 object ScalaTutorial {
 
 	def main(args: Array[String]) {
-		var i = 0 
+		// var i = 0 
 
 		// while (i <= 10) {
 		// 	println(i)
@@ -16,16 +24,54 @@ object ScalaTutorial {
 		// for (i <- 1 to 10) 
 		// 	println(i)
 
-		val randLetters = "ABCSDEFGHIJK"
-
+		// val randLetters = "ABCSDEFGHIJK"
 		// for (i <- 0 until randLetters.length) 
 		// 	println(randLetters(i))
 
-		val aList = List(5, 4, 3, 2, 1)
-
+		// val aList = List(5, 4, 3, 2, 1)
 		// for (i <- aList) {
 		// 	println("List item: " + i)
 		// }
+
+		// var eventList = for { i <- i to 20
+		// 	if (i % 2) == 0 
+		// } yield i
+
+		// for (i <- eventList) {
+		// 	println(i)
+		// }
+
+		// double dimention arrays
+		// for (i <- 1 to 5; j <- 6 to 10) {
+		// 	println("i :" + i)
+		// 	println("j :" + j)
+		// }
+
+		// break and continue equivalent
+		// def printPrimes() {
+		// 	val primeList = List(1, 2, 3, 5, 7, 11)
+
+		// 	for (i <- primeList) {
+		// 		if (i == 11) 
+		// 			return
+
+		// 		if (i != 1)
+		// 			println(i)
+		// 	}
+		// }
+
+		// printPrimes
+
+		// reading lines
+		// var numberGuess  = 0
+
+		// do {
+		// 	print("Guess a number: ")
+		// 	numberGuess = readLine.toInt
+		// } while (numberGuess != 15)
+
+		// printf("You guessed the secret number %d\n", 15)
+
 	}
 
 }
