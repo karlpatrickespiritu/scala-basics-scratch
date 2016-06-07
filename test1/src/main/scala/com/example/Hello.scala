567a87649f04283a24d48b5e43a8d0ce60dec9ab
Developer <developer@VDX-MBA6.local>
2016-06-07 10:38:51 +0800

    first files

---
+++ test1/src/main/scala/com/example/Hello.scala
@@ -0,0 +1,31 @@
+object ScalaTutorial {
+
+	def main(args: Array[String]) {
+		var i = 0 
+
+		// while (i <= 10) {
+		// 	println(i)
+		// 	i += 1
+		// }
+
+		// do {
+		// 	println(i)
+		// 	i += 1
+		// } while (i < 10)
+
+		// for (i <- 1 to 10) 
+		// 	println(i)
+
+		val randLetters = "ABCSDEFGHIJK"
+
+		// for (i <- 0 until randLetters.length) 
+		// 	println(randLetters(i))
+
+		val aList = List(5, 4, 3, 2, 1)
+
+		// for (i <- aList) {
+		// 	println("List item: " + i)
+		// }
+	}
+
+}
